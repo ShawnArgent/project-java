64352e9b57dd4a11ad178340f20a209a7307c304
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-09 11:16:18 -0700

    TypeDefs Models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,50 @@ const typeDefs = gql`
   "Unix time stamp in milliseconds."
   scalar Date
 
+  type User {
+    _id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Coffee {
+    _id: ID!
+    name: String
+    roast: String
+    type: String
+    quantity: Int
+    price: Float
+    tastingProfile: String
+    image: String
+    location: String
+    locationHistory: String
+  }
+
+  type Order {
+    _id: ID!
+    purchaseDate: String
+    coffees: [Coffee]
+  }
+
+  type Recipe {
+    _id: ID!
+    title: String
+    description: String
+    ingredients: String
+    image: String
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: String!
+    user: User!
+  }
+
   type Query {
     "Find the logged in user."
     me: User
@@ -13,20 +57,14 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    createUser(email: String!, password: String!, username: String!): Auth
+    createUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    newOrder(coffees: [ID]!): Order
     login(email: String!, password: String!): Auth
-  }
-
-  type Auth {
-    token: String!
-    user: User!
-  }
-
-  type User {
-    _id: ID!
-    username: String!
-    email: String!
-    lastLogin: Date!
   }
 `;
 
