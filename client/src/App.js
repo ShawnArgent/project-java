abd1d43439a30db56ce3e76c2812e85919def666
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-10 12:20:45 -0700

    Recipe route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import { ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Nav/Navbar';
-import RequireAuth from './components/RequireAuth';
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Nav/Navbar";
+import RequireAuth from "./components/RequireAuth";
 import CartHistory from "./pages/CartHistory";
-import Home from './pages/Home';
-import Login from './pages/Login';
-import ProtectedPageExample from './pages/ProtectedPageExample';
-import SignUp from './pages/SignUp';
-import { client } from './util/apolloClient';
-import { AuthProvider } from './util/auth';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProtectedPageExample from "./pages/ProtectedPageExample";
+import SignUp from "./pages/SignUp";
+import Recipe from "./pages/Recipes";
+import { client } from "./util/apolloClient";
+import { AuthProvider } from "./util/auth";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/carthistory" element={<CartHistory />} />
+            <Route path="/recipe" element={<Recipe />} />
             {/* Use <RequiredAuth> for pages that should only be accessible to a
             user that has logged in.*/}
 
