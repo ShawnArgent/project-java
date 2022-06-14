4d62f5fc296399b38310f0a7d15fb739c5fd8f23
Shawn Argent <kshawn001@gmail.com>
2022-06-13 18:55:00 -0700

    mongo

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import './index.css';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 // import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -17,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+serviceWorker.register();
