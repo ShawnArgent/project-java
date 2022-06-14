8cc2185abd84635b1187bd55c207049918717b67
Shawn Argent <kshawn001@gmail.com>
2022-06-14 01:06:03 -0700

    state

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import './index.css';
 import App from './App';
--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_RECIPES } from '../util/queries';
 
